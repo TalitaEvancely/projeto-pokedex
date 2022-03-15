5d098d3a90ca3e7573983966b9f139095a3fa9c9
Talita Evancely <93993715+TalitaEvancely@users.noreply.github.com>
2022-03-15 17:33:42 -0300

    main

--- src/js/index.js
+++ src/js/index.js
@@ -6,15 +6,7 @@ Para isso vamos precisar trabalharcom 2 elementos:
 
 Precisamos criar 2 variaveis JS para trabalhar com os elementos da tela.
 
-Vamos precisar trabalhar com um evento de crique feito pelo usuário na listagem pokémon.
-
-1 - Remover a classe aberto só do cartão que está aberto.
-2 - Ao clicar em um pokémon da listagem pegamos o id dele para saber qual cartão mostrar.
-3- Remover a classe 'ativo' que marca o pokémon selecionado.
-4 - Adionar a classe ativo no item selecionado.
-*/
-
-/*Precisamos criar 2 variaveis JS para trabalhar com os elementos da tela.
+Precisamos criar 2 variaveis JS para trabalhar com os elementos da tela.
 querySelectorALL busca todos os elementos
 */
 
@@ -22,18 +14,18 @@ querySelectorALL busca todos os elementos
  const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
 
  listaSelecaoPokemons.forEach(pokemon => {
-     //Vamos precisar trabalhar com um evento de crique feito pelo usuário na listagem pokémon.
+     //Vamos precisar trabalhar com um evento de clique feito pelo usuário na listagem pokémon.
+  
      pokemon.addEventListener('click',() => {
-     //1 - Remover a classe aberto só do cartão que está aberto.
+   
      const cartaoPokemonAberto = document.querySelector('.aberto')
      cartaoPokemonAberto.classList.remove ('aberto')
 
-     //2 - Ao clicar em um pokémon da listagem pegamos o id dele para saber qual cartão mostrar.
-    const idPokemonSelecionado =  pokemon.attributes.id.value
+   const idPokemonSelecionado =  pokemon.attributes.id.value
 
     const cartaoPokemonParaAbrir = document.getElementById('cartao-' + idPokemonSelecionado)
     cartaoPokemonParaAbrir.classList.add('aberto')
-    //3- Remover a classe 'ativo' que marca o pokémon selecionado.
+ 
     const pokemonAtivoNaListagem = document.querySelector('.ativo')
     pokemonAtivoNaListagem.classList.remove('ativo')
 
